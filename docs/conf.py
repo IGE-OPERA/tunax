95447807d449919f25d17f2ac29e648ac8227756
Gabriel Mouttapa <gabriel.mouttapa@gmail.com>
2026-09-08 15:36:27 +0200

    Bump to versio 0.2.12

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = 'tunax'
 author = 'Gabriel Mouttapa'
 copyright = '2024, Gabriel Mouttapa, Julien Le Sommer'
 version = '0.2'
-release = '0.2.11'
+release = '0.2.12'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
